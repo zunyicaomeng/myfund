066575f6e23b420a61d617f7ab18d99dcc5681fc
March <961374753@qq.com>
2017-10-20 13:07:13 +0800

    增加首页动态时间

--- src/main/webapp/js/safeTime.js
+++ src/main/webapp/js/safeTime.js
@@ -1,24 +1,49 @@
-function safeTime() {
-	            var today=new Date()
-				var y=today.getFullYear()
-				var M=today.getMonth()
-				var d=today.getDay()
-				var h=today.getHours()
-				var m=today.getMinutes()
-				var s=today.getSeconds()
-// add a zero in front of numbers<10
-y=checkTime(y)
-M=checkTime(M+1)
-d=checkTime(d+15)
-m=checkTime(m)
-s=checkTime(s)
-document.getElementById('txt').innerHTML=y+"年"+M +"月" + d + "日" + h+"时"+m+"分"+s +"秒"
-t=setTimeout('startTime()',500)
-}
-
-function checkTime(i)
-{
-	if (i<10) 
-		{i="0" + i}
-	return i
-}
+function getTime(){
+        setInterval("safeSec.innerHTML=getTheSecend();", 1000);
+        setInterval("safeMin.innerHTML=getTheMinute();", 1000);
+        setInterval("safeHour.innerHTML=getTheHour();", 1000);
+        setInterval("safeDay.innerHTML=getTheDay();", 1000);
+        setInterval("safeYear.innerHTML=getTheYear();", 1000);
+    }
+     //获取Year
+    function getTheYear() {
+        var t = new Date() - new Date(2017, 7, 31);
+        var y = Math.floor(t / 1000 / 60 / 60 / 24 / 30 / 12);
+        return y ;
+     }
+    //获取Day
+    function getTheDay() {
+        var t = new Date() - new Date(2017, 7, 31);
+        var d = Math.floor(t / 1000 / 60 / 60 / 24 );
+        return d ;
+    }
+    //获取Hour
+    function getTheHour() {
+        var t = new Date() - new Date(2017, 7, 31);
+        var d = Math.floor(t / 1000 / 60 / 60 / 24);
+        t -= d * 1000 * 60 * 60 * 24;
+        var h = Math.floor(t / 1000 / 60 / 60);
+        return h ;
+    }
+    //获取Minute
+    function getTheMinute() {
+        var t = new Date() - new Date(2017, 7, 31);
+        var d = Math.floor(t / 1000 / 60 / 60 / 24);
+        t -= d * 1000 * 60 * 60 * 24;
+        var h = Math.floor(t / 1000 / 60 / 60);
+        t -= h * 1000 * 60 * 60;
+        var m = Math.floor(t / 1000 / 60);
+        return m ;
+    }
+    //获取Secend
+    function getTheSecend() {
+        var t = new Date() - new Date(2017, 7, 31);
+        var d = Math.floor(t / 1000 / 60 / 60 / 24);
+        t -= d * 1000 * 60 * 60 * 24;
+        var h = Math.floor(t / 1000 / 60 / 60);
+        t -= h * 1000 * 60 * 60;
+        var m = Math.floor(t / 1000 / 60);
+        t -= m * 1000 * 60;
+        var s = Math.floor(t / 1000);
+        return s;
+    }
