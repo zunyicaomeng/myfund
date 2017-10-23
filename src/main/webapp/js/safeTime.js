4db708473bee2c709c54f8e03598064c8ee230c0
March <961374753@qq.com>
2017-10-23 10:26:27 +0800

    登录页面时间更改

--- src/main/webapp/js/safeTime.js
+++ src/main/webapp/js/safeTime.js
@@ -7,37 +7,49 @@ function getTime(){
     }
      //获取Year
     function getTheYear() {
-        var t = new Date() - new Date(2017, 7, 31);
-        var y = Math.floor(t / 1000 / 60 / 60 / 24 / 30 / 12);
+        var t = new Date() - new Date(2017, 10, 16);
+        var y = Math.floor(t / 1000 / 60 / 60 / 24 / 30 / 12)+1;
+        if (y<10) {
+            y="0" + y;
+        }
         return y ;
      }
     //获取Day
     function getTheDay() {
-        var t = new Date() - new Date(2017, 7, 31);
-        var d = Math.floor(t / 1000 / 60 / 60 / 24 );
+        var t = new Date() - new Date(2017, 10, 16);
+        var d = Math.floor(t / 1000 / 60 / 60 / 24 )+31;
+        if (d<10) {
+            d="0" +d;
+        }
         return d ;
     }
     //获取Hour
     function getTheHour() {
-        var t = new Date() - new Date(2017, 7, 31);
+        var t = new Date() - new Date(2017, 10, 16);
         var d = Math.floor(t / 1000 / 60 / 60 / 24);
         t -= d * 1000 * 60 * 60 * 24;
         var h = Math.floor(t / 1000 / 60 / 60);
+        if (h<10) {
+            h="0" +h;
+        }
         return h ;
     }
     //获取Minute
     function getTheMinute() {
-        var t = new Date() - new Date(2017, 7, 31);
+        var t = new Date() - new Date(2017, 10, 16);
         var d = Math.floor(t / 1000 / 60 / 60 / 24);
         t -= d * 1000 * 60 * 60 * 24;
         var h = Math.floor(t / 1000 / 60 / 60);
         t -= h * 1000 * 60 * 60;
         var m = Math.floor(t / 1000 / 60);
+        if (m<10) {
+            m="0" +m;
+        }
         return m ;
     }
     //获取Secend
     function getTheSecend() {
-        var t = new Date() - new Date(2017, 7, 31);
+        var t = new Date() - new Date(2017, 10, 16);
         var d = Math.floor(t / 1000 / 60 / 60 / 24);
         t -= d * 1000 * 60 * 60 * 24;
         var h = Math.floor(t / 1000 / 60 / 60);
@@ -45,5 +57,8 @@ function getTime(){
         var m = Math.floor(t / 1000 / 60);
         t -= m * 1000 * 60;
         var s = Math.floor(t / 1000);
+        if (s<10) {
+            s="0" +s;
+        }
         return s;
     }
