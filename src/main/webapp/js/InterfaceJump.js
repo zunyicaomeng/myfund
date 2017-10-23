11fad7552d2e422c129c9e64d7f6abb9fd506552
zhao， yu <2560138574@qq.com>
2017-10-23 19:42:29 +0800

    admin界面

---
+++ src/main/webapp/js/InterfaceJump.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gfsm on 2017/10/17.
+ */
+function FreezeUser(){
+    document.location = "/My-Fund/animal/FreezeUser.jsp";
+}
+function  MondifyUser() {
+    document.location ="/My-Fund/animal/ModifyUser.jsp"
+
+}
+function  UserDelete() {
+    document.location = "/My-Fund/animal/UserDelete.jsp"
+
+}
+function  anim() {
+    document.location = "/My-Fund/animal/anim.jsp"
+
+}
+
